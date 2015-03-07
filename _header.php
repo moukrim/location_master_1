4abed1864f29e67d7fe5f77f6104bfcc37bf6ad9
moukrim <moukrim_yassine@hotmail.fr>
2015-03-07 17:52:23 +0100

    Revert "1er commit"
    This reverts commit 58afe86368e26172585340f52a4dffd0e1e633e4.

--- _header.php
+++
@@ -1,8 +0,0 @@
-<?php
- $cnx = @mysql_connect('localhost', 'root', '') ;
- //sélection de la base de données
- $db  = mysql_select_db('location') ;
-require 'panier.class.php';
-$panier=new panier();
-
-?>
