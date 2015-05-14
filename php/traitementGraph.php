86cae8b97f0dd7395531377a914486762e2997e4
cedric777 <cedric.teramo@gmail.com>
2015-05-14 19:35:00 +0200

    Update traitementGraph.php

--- php/traitementGraph.php
+++ php/traitementGraph.php
@@ -1,23 +1,29 @@
 <?php
-
+//cedric teramo
+/*#######################################################*/
+/* Page qui permet de recupérer et de d'additonner les sommes rapporter par vehicule 
+et de les renvoyer dans un tableau avec comme index l'id du vehicule correspondant*/
+/*#######################################################*/
   $cnx = @mysql_connect('localhost', 'root', '') ;
   $db_selected = mysql_select_db('location');    
- 
+ //on recupère chauqe id de vehicule
 $result=mysql_query("SELECT DISTINCT idVehicule FROM reservation");
 
+//on declare 2 tableaux
 $rows = array();
 $som=array();
 while($r = mysql_fetch_array($result)) {
-
+	//on recupère tout les prix par id de vehicule
 	$res=mysql_query("SELECT prixFinale FROM reservation where idVehicule=".$r['idVehicule']."");
 
 	while($re = mysql_fetch_array($res))
 	{
-
+			//on effectue la somme des prix pour chaque vehicule
 			array_push($som, $re['prixFinale']);
 	}
-
+	//on met dans le tableau rows la somme totale avec comme index l'id du vehicule
 	$rows[$r['idVehicule']]=array_sum($som);
+	//on reinitialse le tableau som
 	$som=array();
 
 }
