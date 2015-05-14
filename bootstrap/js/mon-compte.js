274a28e13a3607469c181fe89ea7d3e065be9b94
moukrim <moukrim_yassine@hotmail.fr>
2015-05-14 23:07:44 +0200

    10eme commit
    avancement du code

--- bootstrap/js/mon-compte.js
+++ bootstrap/js/mon-compte.js
@@ -1,13 +1,10 @@
 //yassine
 var session;
 
-/*#######################################################*/
-/* Gérer le compte utilisateur*/
-/*#######################################################*/
-$(document).ready(function () {   
 
-  //avoir id utilisateur pour gérer son compte
-  session=$("#session").val();
+function recupInfoProfile(){
+
+ session=$("#session").val();
    $.ajax({
         url: "../php/profil-moncompte.php",
         method: "POST",
@@ -16,34 +13,13 @@ $(document).ready(function () {
         .done(function(msg) {
           tableau=$.parseJSON(msg);
           $("#affiche-profile").append('<tr><td>'+tableau.nom+'</td><td>'+tableau.prenom+'</td><td>'+tableau.adrMail+'</td><td>'+tableau.mdp+'</td></tr>');
-      });    
+      }); 
 
+}
 
-  $.ajax({
-        url: "../php/info-moncompte.php",
-        method: "POST",
-        data: { id : session  },
-        })
-        .done(function(msg) {
-          array=$.parseJSON(msg);
+function clickAnnulReserv(index){
 
-        
-
-          $.each(array, function( index, value ) {
-
-              $("#affiche-historique").append('<tr id="affiche'+index+'"><td> <input type="button" id="btn-historique'+index+'" class="btn btn-primary" value="Infos"></td><td> <input type="button" id="btn-historique-annuler'+index+'" class="btn btn-danger" value="Annuler"></td><td><input type="text" style="display:none;" class="form-control" id="numReservation'+index+'" placeholder="Numéro de réservation"></td>\
-                <td><div id="pas-annul'+index+'"></div> <input type="button" style="display:none;" id="btn-historique-annul-annuler'+index+'" class="btn btn-danger" value="Abandonner"></td><td> <input type="button" style="display:none;" id="btn-historique-confirm-annul'+index+'" class="btn btn-success" value="Confirmer"></td></tr>'); 
-             	  $.each(value, function( ind, val ) {
-             	  	if(ind=='2'){
-             	  	$("#affiche"+index).prepend('<td><img src="'+val+'" style="height:50px;"></td>');
-               	  }else if(ind=='3'){
-                    $("#affiche"+index).prepend('<input type="hidden" id="idh'+index+'" value="'+val+'" ></td>');
-                  }else{
-                    $("#affiche"+index).prepend('<td>'+val+'</td>');
-                  }
-             
-                }); 
-                 $('#btn-historique-annuler'+index).click(function(){
+ $('#btn-historique-annuler'+index).click(function(){
                  $('#btn-historique-annul-annuler'+index).show('slow');
                  $('#btn-historique-confirm-annul'+index).show('slow');
                  $('#numReservation'+index).show('slow');
@@ -51,15 +27,24 @@ $(document).ready(function () {
               
                    });
 
-                  $('#btn-historique-annul-annuler'+index).click(function(){
+
+}
+
+function clickAnnulAnnulation(index){
+
+   $('#btn-historique-annul-annuler'+index).click(function(){
                  $('#numReservation'+index).hide('slow');
                  $('#btn-historique-confirm-annul'+index).hide('slow');
                  $(this).hide('slow');
                  $('#btn-historique-annuler'+index).show('slow');
                   
                    });
+}
 
-                   $('#btn-historique-confirm-annul'+index).click(function(){
+
+function confAnnulReserv(index){
+
+ $('#btn-historique-confirm-annul'+index).click(function(){
                      $.ajax({
                       url: "../php/annuler-reservation.php",
                       method: "POST",
@@ -90,7 +75,30 @@ $(document).ready(function () {
                  $('#btn-historique-annuler'+index).show('slow');
                    });
 
-                $("#btn-historique"+index).click(function(){
+
+}
+
+
+function afficheInfoVehiculeReserve(value,index){
+
+     $.each(value, function( ind, val ) {
+                  if(ind=='2'){
+                  $("#affiche"+index).prepend('<td><img src="'+val+'" style="height:50px;"></td>');
+                  }else if(ind=='3'){
+                    $("#affiche"+index).prepend('<input type="hidden" id="idh'+index+'" value="'+val+'" ></td>');
+                  }else{
+                    $("#affiche"+index).prepend('<td>'+val+'</td>');
+                  }
+             
+                });
+
+
+}
+
+
+function afficheInfoReservation(index){
+
+   $("#btn-historique"+index).click(function(){
 
                   $("#affiche-date").empty();
                   var idv =$("#idh"+index).val();
@@ -112,6 +120,37 @@ $(document).ready(function () {
                         });
                                       
                 });
+}
+
+/*#######################################################*/
+/* Gérer le compte utilisateur*/
+/*#######################################################*/
+$(document).ready(function () {   
+
+  //avoir id utilisateur pour gérer son compte
+  recupInfoProfile();
+
+
+  $.ajax({
+        url: "../php/info-moncompte.php",
+        method: "POST",
+        data: { id : session  },
+        })
+        .done(function(msg) {
+          array=$.parseJSON(msg);
+
+        
+
+          $.each(array, function( index, value ) {
+
+              $("#affiche-historique").append('<tr id="affiche'+index+'"><td> <input type="button" id="btn-historique'+index+'" class="btn btn-primary" value="Infos"></td><td> <input type="button" id="btn-historique-annuler'+index+'" class="btn btn-danger" value="Annuler"></td><td><input type="text" style="display:none;" class="form-control" id="numReservation'+index+'" placeholder="Numéro de réservation"></td>\
+                <td><div id="pas-annul'+index+'"></div> <input type="button" style="display:none;" id="btn-historique-annul-annuler'+index+'" class="btn btn-danger" value="Abandonner"></td><td> <input type="button" style="display:none;" id="btn-historique-confirm-annul'+index+'" class="btn btn-success" value="Confirmer"></td></tr>'); 
+             	afficheInfoVehiculeReserve(value,index);       
+              clickAnnulReserv(index);
+              clickAnnulAnnulation(index);
+              confAnnulReserv(index);
+              afficheInfoReservation(index);
+                  
                    	
         });
               
