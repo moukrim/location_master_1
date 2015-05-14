5764443477030d8475d822d4651c0058c090df03
cedric777 <cedric.teramo@gmail.com>
2015-05-14 18:39:17 +0200

    Update ajoutClient.js

--- bootstrap/js/ajoutClient.js
+++ bootstrap/js/ajoutClient.js
@@ -1,14 +1,18 @@
 //cedric teramo
+/*#######################################################*/
+/* Ajouter des clients */
+/*#######################################################*/
 $(document).ready(function() {
 
-//ajout d'un client après le click le bouton ajoutClient
+//ajout d'un client après le click du bouton ajoutClient
 $("#boutonAjoutClient").click(function() {
-
+//récuperation des champs
 var nom=$("#nom");
 var prenom=$("#prenom");
 var adrMail=$("#adrMail");
 var mdp=$("#mdp");
 
+//vérifications champs
 if(nom.val() == '')
 {
 	$("#nom").focus();			
@@ -34,7 +38,7 @@ if(mdp.val() == '')
 	$("#mdpInvalide").html('<span>Champs Mot de passe vide</span>');
 return false;
 }
-
+	//envoi des données vers la page traitementAjoutClient.php
 	var request = $.ajax({
 
 		url: "traitementAjoutClient.php",
@@ -42,21 +46,21 @@ return false;
 		data: {nom:nom.val(), prenom:prenom.val(), adrMail:adrMail.val(), mdp:mdp.val(), },
 
 	});
-
+//recuperation de la reponse
 request.done(function(msg){
 
 console.log(msg);
 
 
 if(msg = "ajout reussie")
-{
+{	//affichage d'un pop up pour confirmer l'ajout
 	$("#modal-ajout-Client").modal("show");
-
+	//reinitialisation des champs
 	$('#nom').val('');
 	$('#prenom').val('');
 	$('#adrMail').val('');
 	$('#mdp').val('');
-
+	//on vide les div après affichage d'une erreur de saisie
 	$('#nomInvalide').empty();
 	$('#prenomInvalide').empty();
 	$('#adrMailInvalide').empty();
